32e2515d8f532460339a82847e65c7e818722f28
aditya patil <105120546+anavguser@users.noreply.github.com>
2024-09-05 02:47:59 +0530

    i did some madness here, ngl

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FileUpload from './components/FileUpload';
 import DataDisplay from './components/DataDisplay';
+import TransactionSummaries from './components/TransactionSummaries';
+import TransactionDetails from './components/TransactionDetails';
 
 const theme = createTheme();
 
@@ -19,15 +22,27 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container maxWidth="lg">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            CSV Processor
-          </Typography>
-          <FileUpload onDataProcessed={handleDataProcessed} />
-          {processedData && <DataDisplay data={processedData} />}
-        </Box>
-      </Container>
+      <Router>
+        <Container maxWidth="lg">
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              CSV Processor
+            </Typography>
+            <FileUpload onDataProcessed={handleDataProcessed} />
+            <Routes>
+              <Route path="/" element={
+                processedData && (
+                  <>
+                    <TransactionSummaries data={processedData} />
+                    <DataDisplay data={processedData} />
+                  </>
+                )
+              } />
+              <Route path="/transaction/:description" element={<TransactionDetails data={processedData} />} />
+            </Routes>
+          </Box>
+        </Container>
+      </Router>
     </ThemeProvider>
   );
 }
